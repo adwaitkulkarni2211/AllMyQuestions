6cf756f8a5b320c7b9c7469f5f171972cabf799b
Adwait Kulkarni <adwaitkulkarni2211@gmail.com>
2020-06-18 17:31:28 +0530

    added remove a node to linked list

---
+++ linkedList/RemoveNodeFromAnyPoint.java
@@ -0,0 +1,30 @@
+package linkedList;
+
+public class RemoveNodeFromAnyPoint {
+	 static SinglyLinkedListNode deleteNode(SinglyLinkedListNode head, int position) {
+	        if(head==null) {
+	            return head;
+	        } else if(head.next==null) {
+	            head=null;
+	            return head;
+	        } else if(position == 0) {
+	            head = head.next;
+	            return head;
+	        } else if(position == 1) {
+	            head.next = head.next.next;
+	            return head;
+	        } else {
+	            SinglyLinkedListNode temp = head;
+	            int i=0;
+	            while(temp.next.next!=null) {                
+	               temp=temp.next;
+	                i++;                
+	                if(i==position-1) {
+	                    temp.next = temp.next.next;
+	                }                
+	            }
+	            return head;
+	        }
+
+	    }
+}
